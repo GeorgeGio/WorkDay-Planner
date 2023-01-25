2cda0e0f991bedbbac465ae13bd0bf2fa28e040e
George <georgeoustapas@gmail.com>
2023-01-25 00:48:06 +0000

    used a for loop to create the buttons

--- script.js
+++ script.js
@@ -1,1 +1,24 @@
-$("#currentDay").text(moment().format("dddd, MMMM Do"));
+var timeInterval = setInterval(function () {
+    $("#currentDay").text(moment().format('MMMM Do YYYY, h:mm:ss a'));
+
+}, 1000)
+
+
+
+
+for (let i = 0; i <=8; i++) {
+
+    $(".container").append(
+        `    
+        <div class="row">
+        ${i+9}
+        <textarea class="col-8"></textarea>
+        
+        <button> Save </button>
+        </div>`)
+    
+     
+
+}
+
+$("textarea").attr("class", "future")
