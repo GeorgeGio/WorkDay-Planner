0e8df6cc31315bf2c745b92f03676afe9410567b
George <georgeoustapas@gmail.com>
2023-01-27 21:17:21 +0000

    added color code to the current time

--- script.js
+++ script.js
@@ -3,23 +3,35 @@ var timeInterval = setInterval(function () {
 
 }, 1000)
 
-
-function someFunction () {
+let currentHour = moment().hour();
+console.log(currentHour);
+function someFunction() {
     // const items = [];
     new Array(9).fill().forEach((acc, index) => {
-      let hour = moment( {hour: index + 9} ).format('h:mm A');
-      $(".container").append(
-        `    
+        let hour = index +9;
+        let formattedHour = moment({ hour: hour }).format('h:mm A');
+        $(".container").append(
+            `    
         <div class="row">
-        <span class="col-2"> ${hour} </span>
-        <textarea class="col-8"></textarea>
+        <span class="col-2"> ${formattedHour} </span>
+        <textarea class="col-8 ${setTimeOfDay(hour) }"></textarea>
         
         <button> Save </button>
         </div>`)
-    
+
     })
-  
-  }
+
+}
+function setTimeOfDay(hour) {
+    if (hour > currentHour){
+        return "future";
+    }
+    else if (hour < currentHour){
+        return "past"
+
+    }
+    return "present"    
+}
 
 someFunction();
 // $("textarea").attr("class", "future")
